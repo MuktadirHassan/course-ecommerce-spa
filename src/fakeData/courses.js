5703a5922172fda98b45e3da181c8d640d11ca7c
MuktadirHassan <tamimmuktadir@gmail.com>
2020-08-27 11:09:37 +0600

    created project, added fakeData

---
+++ src/fakeData/courses.js
@@ -0,0 +1,88 @@
+var courses = [
+    {
+        id:1,
+        name: 'Programming Foundations: Algorithms',
+        author: 'Joe Marini',
+        price:59.99,
+        duration: '1h 45m'
+    },
+    {
+        id:2,
+        name: 'Learning Java',
+        author: 'Kathy Hodge',
+        price:50.99,
+        duration: '2h 15m'
+    },
+    {
+        id:3,
+        name: 'Learning Python',
+        author: 'Joe Marini',
+        price:59.99,
+        duration: '2h 45m'
+    },
+    {
+        id:4,
+        name: 'Python Essentials Training',
+        author: 'Bill Warren',
+        price:99.99,
+        duration: '4h 45m'
+    },
+    {
+        id:5,
+        name: 'Learning SQL Programming',
+        author: 'Scott Simpson',
+        price:29.99,
+        duration: '1h 25m'
+    },
+    {
+        id:6,
+        name: 'Programming Foundations: Real-World Examples',
+        author: 'Joe Marini',
+        price:159.99,
+        duration: '3h 45m'
+    },
+    {
+        id:7,
+        name: 'Python: Data Analysis (2015)',
+        author: 'Joe Biden',
+        price:59.99,
+        duration: '2h 16m'
+    },
+    {
+        id:8,
+        name: 'Access 2016 Essential Training',
+        author: 'Adam Wilbert',
+        price:30.99,
+        duration: '6h 45m'
+    },
+    {
+        id:9,
+        name: 'Docker and Kubernetes: The Complete Guide',
+        author: 'Luca Gennari',
+        price:9.99,
+        duration: '2h 45m'
+    },
+    {
+        id:10,
+        name: 'Advanced CSS and Sass: Flexbox, Grid and Animations!',
+        author: 'Josh Hamidi',
+        price:9.99,
+        duration: '1h 45m'
+    },
+    {
+        id:11,
+        name: 'The Complete 2020 Web Development Bootcamp',
+        author: 'Dr. Angela Yu',
+        price:39.99,
+        duration: '9h 15m'
+    },
+    {
+        id:12,
+        name: 'How to Work with Interior Design Styles Like a Pro',
+        author: 'Erika Fogleman',
+        price:99.99,
+        duration: '5h 45m'
+    }
+]
+
+export default courses;
